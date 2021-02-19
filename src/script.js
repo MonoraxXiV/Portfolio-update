03839ec4b41428747b29b237b17b5d6662cac862
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2021-02-19 16:29:36 +0100

    added scripts for collabsibles, might have to remove the scripts for the buttons

---
+++ src/script.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+    document.getElementById("myDropdown2").classList.toggle("show");
+    document.getElementById("myDropdown3").classList.toggle("show");
+    document.getElementById("myDropdownColab").classList.toggle("show");
+    document.getElementById("myDropdownOther").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
