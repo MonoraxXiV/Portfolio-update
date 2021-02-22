e0ad1eb1f6deb9937d88e2c22496382887785333
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2021-02-22 11:00:28 +0100

    removed the dropdown button logic, added extra section for collapsible two for extra contrast to show difference

--- src/script.js
+++ src/script.js
@@ -1,30 +1,24 @@
-function myFunction() {
-    document.getElementById("myDropdown1").classList.toggle("show");
-    document.getElementById("myDropdown2").classList.toggle("show");
-    document.getElementById("myDropdown3").classList.toggle("show");
-    document.getElementById("myDropdownColab").classList.toggle("show");
-    document.getElementById("myDropdownOther").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+var coll2 = document.getElementsByClassName("collapsible2");
+var j;
+
+for (j = 0; j < coll2.length; j++) {
+    coll2[j].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
         if (content.style.display === "block") {
